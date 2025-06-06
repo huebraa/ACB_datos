f02d17487d7689b624b854c7c943a85714476561
huebraa <huebra.asier@gmail.com>
2025-06-06 12:46:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib.colors import to_hex
-from sklearn.cluster import KMeans
-from sklearn.preprocessing import StandardScaler, MinMaxScaler
-from sklearn.decomposition import PCA
-from scipy.cluster.hierarchy import linkage, dendrogram
 
 
 # Cargar datos
