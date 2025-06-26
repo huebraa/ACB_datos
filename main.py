b64fa1d89a4d1edf66c19cdb3e38a5e1c0d9cbc5
huebraa <huebra.asier@gmail.com>
2025-06-26 09:51:29 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.patheffects as path_effects
 from matplotlib.colors import to_hex
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
